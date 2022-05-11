68f94533e957211cfac62c7e7e7042003a637788
Ziggy <68012775+Ziggy434@users.noreply.github.com>
2022-05-11 15:07:23 +0800

    Update dice_cl.lua

--- dice_cl.lua
+++ dice_cl.lua
@@ -1,6 +1,14 @@
 RegisterNetEvent('proxDiceResult')
-AddEventHandler('proxDiceResult', funciton(id, result)
-    
+AddEventHandler('proxDiceResult', function(id, result)
+    local playerId = PlayerId()
+    local senderId = GetPlayerFromServerId(id)
+    local senderName = GetPlayerName(id)
+
+    if senderId == playerId then
+        TriggerEvent('chatMessage', '^4' .. senderName .. ' rolled a ' .. result)
+    elseif GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(playerId)), GetEntityCoords(GetPlayerPed(senderId)), true) < 19.999 then
+        TriggerEvent('chatMessage', '^4' .. senderName .. ' rolled a ' .. result)
+    end
 end)
 
 function notify(text)
@@ -8,3 +16,4 @@ function notify(text)
     AddTextComponentString(text)
     DrawNotification(false, false)
 end
+
